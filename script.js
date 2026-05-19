ac10d9474caa60bddbd98fe7fedd305dac49fc63
Chris Watson <chriswatson6675@gmail.com>
2026-05-19 22:28:28 +0000

    Fix coffee button - use window.location.href instead of window.open

--- script.js
+++ script.js
@@ -416,15 +416,14 @@ function searchKebabs() {
 }
 
 function searchCoffee() {
-  console.log('Coffee button clicked');
   if (!currentLoc || !currentLoc.lat || !currentLoc.lng) {
     alert('Map not ready yet. Try again in a moment.');
     return;
   }
   var lat = currentLoc.lat;
   var lng = currentLoc.lng;
-  console.log('Opening coffee search at', lat, lng);
-  window.open('https://www.google.com/maps/search/coffee/@' + lat + ',' + lng + ',15z', '_blank');
+  var url = 'https://www.google.com/maps/search/coffee/@' + lat + ',' + lng + ',15z';
+  window.location.href = url;
 }
 
 function useGPS() {
